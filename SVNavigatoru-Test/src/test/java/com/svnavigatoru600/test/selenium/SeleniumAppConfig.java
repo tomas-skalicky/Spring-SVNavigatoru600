4c27a3f4027fb87b9aab877e56828309e9e5ee8c
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-11-22 23:14:25 +0100

    comment refactored

--- SVNavigatoru-Test/src/test/java/com/svnavigatoru600/test/selenium/SeleniumAppConfig.java
+++ SVNavigatoru-Test/src/test/java/com/svnavigatoru600/test/selenium/SeleniumAppConfig.java
@@ -1,14 +1,14 @@
 package com.svnavigatoru600.test.selenium;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 /**
- * {@link org.springframework.context.ApplicationContext Application context} for Selenium tests. It creates necessary
- * {@link Bean beans}.
+ * {@link ApplicationContext Application context} for Selenium tests. It creates necessary {@link Bean beans}.
  * 
  * @author <a href="mailto:tomas.skalicky@gfk.com">Tomas Skalicky</a>
  */
