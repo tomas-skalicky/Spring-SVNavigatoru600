fec96812c47af4cde312e95087770a9725c23a8f
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-11-22 23:04:04 +0100

    test categories refactored

---
+++ SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/test/category/FastTests.java
@@ -0,0 +1,9 @@
+package com.svnavigatoru600.test.category;
+
+/**
+ * Implemented by tests of which evaluation is fast (<code>&lt;= 1 sec</code>).
+ * 
+ * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
+ */
+public interface FastTests {
+}
