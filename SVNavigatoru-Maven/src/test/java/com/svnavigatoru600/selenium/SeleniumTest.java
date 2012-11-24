f301ba2ad0bf1482d0196ea3d9912f23c184eefa
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-11-24 21:57:15 +0100

    comment of the logIn method improved

--- SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/selenium/SeleniumTest.java
+++ SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/selenium/SeleniumTest.java
@@ -68,8 +68,11 @@ public abstract class SeleniumTest {
 	}
 
 	/**
-	 * Signs in the application with the given credentials. Do not test, whether the login in ended up with a success,
+	 * <p>
+	 * Signs in to the application with the given credentials. Do not test, whether the login ended up with a success,
 	 * or not.
+	 * </p>
+	 * <b>PRECONDITION:</b> The current location is the login page of the application.
 	 * 
 	 * @param login
 	 * @param password
