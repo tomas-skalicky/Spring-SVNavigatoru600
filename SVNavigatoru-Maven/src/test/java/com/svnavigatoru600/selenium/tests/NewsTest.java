6c293edc0dd7af4221d98255a50ce0cdd3bea64c
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-11-24 23:07:50 +0100

    selenium tests migrated to the selenium 2; confirmation dialog commented out

--- SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/selenium/tests/NewsTest.java
+++ SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/selenium/tests/NewsTest.java
@@ -2,16 +2,21 @@ package com.svnavigatoru600.selenium.tests;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.util.Assert;
 
+import com.svnavigatoru600.selenium.SeleniumTest;
 import com.svnavigatoru600.test.category.SeleniumTests;
-
 
 /**
  * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
  * @see EmailTest
  */
 @Category(SeleniumTests.class)
-public class NewsTest extends AbstractTestCase {
+public class NewsTest extends SeleniumTest {
 
 	private final String XPATH_NEWS_SECTION = "//*[@id='nav']/li[1]/a[1]";
 
@@ -31,23 +36,6 @@ public class NewsTest extends AbstractTestCase {
 
 	private final String XPATH_FIRST_NEWS_DELETE = "//*[@id='newsList']/*[@class='post'][1]//*[@class='controls'][1]/a[2]";
 
-	private String getElementInfo(String xpath, String info) {
-		StringBuffer builder = new StringBuffer("selenium.get");
-		builder.append(info);
-		builder.append("(\"");
-		builder.append(xpath);
-		builder.append("\")");
-		return builder.toString();
-	}
-
-	private String getText(String xpath) {
-		return this.getElementInfo(xpath, "Text");
-	}
-
-	private String getValue(String xpath) {
-		return this.getElementInfo(xpath, "Value");
-	}
-
 	@Test
 	public void testAddEditDelete() throws Exception {
 		this.logIn();
@@ -58,60 +46,86 @@ public class NewsTest extends AbstractTestCase {
 	}
 
 	private void logIn() {
-		selenium.open("/SVNavigatoru/prihlaseni/");
-		selenium.type("id=login", "skalicky.tomas@gmail.com");
-		selenium.type("id=password", "t");
-		selenium.click("id=rememberMe");
+		final WebDriver browserDriver = this.getBrowserDriver();
 
-		selenium.click("css=input[type='submit']");
-		selenium.waitForPageToLoad(MAX_WAIT_TIME_IN_MS);
+		browserDriver.findElement(By.id("login")).sendKeys("skalicky.tomas@gmail.com");
+		browserDriver.findElement(By.id("password")).sendKeys("t");
+		browserDriver.findElement(By.id("rememberMe")).click();
+		browserDriver.findElement(By.cssSelector("input[type='submit']")).click();
+		this.waitForPageUrl(browserDriver, ".*/novinky/");
 	}
 
 	private void createNewNews() {
+		final WebDriver browserDriver = this.getBrowserDriver();
+
 		// CLUE: Use a Firefox extension called "XPath Checker" to find out a xpath of a certain element.
-		selenium.click("xpath=" + XPATH_NEW_NEWS_LINK);
-		selenium.waitForCondition(this.getValue(XPATH_NEW_NEWS_SUBMIT) + " == 'Přidat novinku'", MAX_WAIT_TIME_IN_MS);
+		browserDriver.findElement(By.xpath(XPATH_NEW_NEWS_LINK)).click();
+		Assert.isTrue((new WebDriverWait(browserDriver, DEFAULT_TIMEOUT_IN_SECONDS,
+				DEFAULT_SLEEP_BETWEEN_POLLS_IN_MILLISECONDS)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return "Přidat novinku".equals(NewsTest.this.getAttributeValue(XPATH_NEW_NEWS_SUBMIT));
+			}
+		}));
 
 		final String newTitle = "New News";
-		selenium.type("xpath=" + XPATH_NEW_NEWS_TITLE, newTitle);
+		browserDriver.findElement(By.xpath(XPATH_NEW_NEWS_TITLE)).sendKeys(newTitle);
 
 		final String newText = "New Text";
-		selenium.type("xpath=" + XPATH_NEW_NEWS_TEXT, "<p>" + newText + "</p>");
+		browserDriver.findElement(By.xpath(XPATH_NEW_NEWS_TEXT)).sendKeys("<p>" + newText + "</p>");
 
-		selenium.click("css=input[type='submit']");
-		String condition = this.getText(XPATH_FIRST_NEWS_TITLE) + " == '" + newTitle + "' && "
-				+ this.getText(XPATH_FIRST_NEWS_TEXT) + " == '" + newText + "'";
-		selenium.waitForCondition(condition, MAX_WAIT_TIME_IN_MS);
+		browserDriver.findElement(By.cssSelector("input[type='submit']")).click();
+		Assert.isTrue((new WebDriverWait(browserDriver, DEFAULT_TIMEOUT_IN_SECONDS,
+				DEFAULT_SLEEP_BETWEEN_POLLS_IN_MILLISECONDS)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return newTitle.equals(NewsTest.this.getElementText(XPATH_FIRST_NEWS_TITLE))
+						&& newText.equals(NewsTest.this.getElementText(XPATH_FIRST_NEWS_TEXT));
+			}
+		}));
 	}
 
 	private void editNewNews() {
-		selenium.click("xpath=" + XPATH_FIRST_NEWS_EDIT);
-		selenium.waitForCondition(this.getValue(XPATH_EDIT_NEWS_SUBMIT) + " == 'Uložit změny'", MAX_WAIT_TIME_IN_MS);
+		final WebDriver browserDriver = this.getBrowserDriver();
+
+		browserDriver.findElement(By.xpath(XPATH_FIRST_NEWS_EDIT)).click();
+		Assert.isTrue((new WebDriverWait(browserDriver, DEFAULT_TIMEOUT_IN_SECONDS,
+				DEFAULT_SLEEP_BETWEEN_POLLS_IN_MILLISECONDS)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return "Uložit změny".equals(NewsTest.this.getAttributeValue(XPATH_EDIT_NEWS_SUBMIT));
+			}
+		}));
 
 		final String newTitle = "Edited News";
-		selenium.type("xpath=" + XPATH_EDIT_NEWS_TITLE, newTitle);
+		browserDriver.findElement(By.xpath(XPATH_EDIT_NEWS_TITLE)).sendKeys(newTitle);
 
 		final String newTextWithFormating = "<p>Edited Text</p>";
-		selenium.type("xpath=" + XPATH_EDIT_NEWS_TEXT, newTextWithFormating);
+		browserDriver.findElement(By.xpath(XPATH_EDIT_NEWS_TEXT)).sendKeys(newTextWithFormating);
 
-		selenium.click("css=input[type='submit']");
-		String condition = "selenium.isVisible(\"xpath=" + XPATH_SUCCESS_EDIT_MESSAGE + "\")";
-		selenium.waitForCondition(condition, MAX_WAIT_TIME_IN_MS);
+		browserDriver.findElement(By.cssSelector("input[type='submit']")).click();
+		Assert.isTrue((new WebDriverWait(browserDriver, DEFAULT_TIMEOUT_IN_SECONDS,
+				DEFAULT_SLEEP_BETWEEN_POLLS_IN_MILLISECONDS)).until(new ExpectedCondition<Boolean>() {
+			public Boolean apply(WebDriver driver) {
+				return driver.findElement(By.xpath(XPATH_SUCCESS_EDIT_MESSAGE)).isDisplayed();
+			}
+		}));
 
-		selenium.click("xpath=" + XPATH_NEWS_SECTION);
-		selenium.waitForPageToLoad(MAX_WAIT_TIME_IN_MS);
+		browserDriver.findElement(By.xpath(XPATH_NEWS_SECTION)).click();
+		this.waitForPageUrl(browserDriver, ".*/novinky/");
 	}
 
 	private void deleteNewNews() {
-		selenium.click("xpath=" + XPATH_FIRST_NEWS_DELETE);
-		selenium.waitForCondition("selenium.isConfirmationPresent()", MAX_WAIT_TIME_IN_MS);
-		final String confirmation = selenium.getConfirmation();
-		assertTrue("The actual value is: " + confirmation,
-				confirmation.matches("^Opravdu chcete smazat novinku `Edited News`\\?$"));
+		final WebDriver browserDriver = this.getBrowserDriver();
+
+		browserDriver.findElement(By.xpath(XPATH_FIRST_NEWS_DELETE)).click();
+		// browserDriver.waitForCondition("selenium.isConfirmationPresent()", MAX_WAIT_TIME_IN_MS);
+		// final String confirmation = browserDriver.getConfirmation();
+		// assertTrue("The actual value is: " + confirmation,
+		// confirmation.matches("^Opravdu chcete smazat novinku `Edited News`\\?$"));
 	}
 
 	private void logOut() {
-		selenium.click("link=Odhlásit se");
-		selenium.waitForPageToLoad(MAX_WAIT_TIME_IN_MS);
+		final WebDriver browserDriver = this.getBrowserDriver();
+
+		browserDriver.findElement(By.linkText("Odhlásit se")).click();
+		this.waitForPageUrl(browserDriver, ".*/prihlaseni/");
 	}
 }
