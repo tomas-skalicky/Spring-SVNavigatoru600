73b53ad322e84774058817b2afeba513f95cf425
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-12-19 21:10:30 +0100

    mybatis introduced

---
+++ SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/repository/MapperInterface.java
@@ -0,0 +1,11 @@
+package com.svnavigatoru600.repository;
+
+/**
+ * Annotated MyBatis mapper interfaces. Is used together with
+ * {@link org.mybatis.spring.mapper.MapperScannerConfigurer MapperScannerConfigurer} which scans given
+ * packages for mappers.
+ * 
+ * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
+ */
+public @interface MapperInterface {
+}
