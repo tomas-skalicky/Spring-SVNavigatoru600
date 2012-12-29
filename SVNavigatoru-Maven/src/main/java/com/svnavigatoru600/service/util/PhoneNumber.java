1ea074edf00dc23e7f88e5375b6a676ade52b58a
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-12-29 13:51:52 +0100

    the length of the phone checked

--- SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/service/util/PhoneNumber.java
+++ SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/service/util/PhoneNumber.java
@@ -7,7 +7,14 @@ package com.svnavigatoru600.service.util;
  */
 public final class PhoneNumber {
 
+    /**
+     * The regular expression which is satisfied by valid phone numbers.
+     */
     private static final String VALID_PHONE_NUMBER_REGEXP = "^[+]?(\\d[ ]*){9,}$";
+    /**
+     * The maximal length of the phone number.
+     */
+    private static final int PHONE_MAX_LENGTH = 20;
 
     private PhoneNumber() {
     }
@@ -16,6 +23,16 @@ public final class PhoneNumber {
      * Indicates whether the given <code>phoneNumber</code> is valid.
      */
     public static boolean isValid(String phoneNumber) {
+        if (isTooLong(phoneNumber)) {
+            return false;
+        }
         return phoneNumber.matches(PhoneNumber.VALID_PHONE_NUMBER_REGEXP);
+    }
+
+    /**
+     * Indicates whether the given phone number is too long to be valid.
+     */
+    public static boolean isTooLong(String phoneNumber) {
+        return phoneNumber.length() > PHONE_MAX_LENGTH;
     }
 }
