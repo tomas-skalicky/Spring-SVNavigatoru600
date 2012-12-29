4be6379c4982588b0ecd8682fdf449c1a7a4b0ba
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-12-29 22:07:24 +0100

    NewsField introduced -> refactoring

--- SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/repository/news/impl/direct/NewsRowMapper.java
+++ SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/repository/news/impl/direct/NewsRowMapper.java
@@ -3,44 +3,28 @@ package com.svnavigatoru600.repository.news.impl.direct;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.jdbc.core.RowMapper;
 
 import com.svnavigatoru600.domain.News;
-import com.svnavigatoru600.repository.users.impl.direct.UserRowMapper;
+import com.svnavigatoru600.repository.news.impl.NewsField;
 
 /**
- * For more information, see {@link UserRowMapper}.
+ * For more information, see {@link com.svnavigatoru600.repository.users.impl.direct.UserRowMapper
+ * UserRowMapper}.
  * 
  * @author Tomas Skalicky
  */
 public class NewsRowMapper implements RowMapper<News> {
 
-    private static final Map<String, String> PROPERTY_COLUMN_MAP;
-
-    static {
-        PROPERTY_COLUMN_MAP = new HashMap<String, String>();
-        PROPERTY_COLUMN_MAP.put("id", "id");
-        PROPERTY_COLUMN_MAP.put("title", "title");
-        PROPERTY_COLUMN_MAP.put("text", "text");
-        PROPERTY_COLUMN_MAP.put("creationTime", "creation_time");
-        PROPERTY_COLUMN_MAP.put("lastSaveTime", "last_save_time");
-    }
-
-    public static String getColumn(String propertyName) {
-        return NewsRowMapper.PROPERTY_COLUMN_MAP.get(propertyName);
-    }
-
     @Override
     public News mapRow(ResultSet rs, int rowNum) throws SQLException {
         News news = new News();
-        news.setId(rs.getInt(NewsRowMapper.getColumn("id")));
-        news.setTitle(rs.getString(NewsRowMapper.getColumn("title")));
-        news.setText(rs.getString(NewsRowMapper.getColumn("text")));
-        news.setCreationTime(new Date(rs.getTimestamp(NewsRowMapper.getColumn("creationTime")).getTime()));
-        news.setLastSaveTime(new Date(rs.getTimestamp(NewsRowMapper.getColumn("lastSaveTime")).getTime()));
+        news.setId(rs.getInt(NewsField.id.getColumnName()));
+        news.setTitle(rs.getString(NewsField.title.getColumnName()));
+        news.setText(rs.getString(NewsField.text.getColumnName()));
+        news.setCreationTime(new Date(rs.getTimestamp(NewsField.creationTime.getColumnName()).getTime()));
+        news.setLastSaveTime(new Date(rs.getTimestamp(NewsField.lastSaveTime.getColumnName()).getTime()));
         return news;
     }
 }
