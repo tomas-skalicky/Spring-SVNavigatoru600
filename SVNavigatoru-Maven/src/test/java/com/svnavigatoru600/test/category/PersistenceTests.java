d83edc983d2ad195359d5e54a26ba82f24f77015
Tomas Skalicky <skalicky.tomas@gmail.com>
2012-12-29 18:37:06 +0100

    PersistenceTests category added

---
+++ SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/test/category/PersistenceTests.java
@@ -0,0 +1,9 @@
+package com.svnavigatoru600.test.category;
+
+/**
+ * Implemented by tests which test the persistence layer.
+ * 
+ * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
+ */
+public interface PersistenceTests {
+}
