e61847da3970c6c484e420ac9a26a0c812797867
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-01-02 19:44:49 +0100

    comments refactored

--- SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/repository/records/DocumentRecordDao.java
+++ SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/repository/records/DocumentRecordDao.java
@@ -10,15 +10,17 @@ import com.svnavigatoru600.repository.MapperInterface;
 public interface DocumentRecordDao {
 
     /**
-     * Returns a {@link AbstractDocumentRecord} stored in the repository which has the given <code>ID</code>.
+     * Returns a {@link AbstractDocumentRecord} stored in the repository which has the given ID.
+     * <p>
+     * The <em>Blob</em> file is loaded as well.
      */
     AbstractDocumentRecord findById(int documentId);
 
     /**
-     * Returns a {@link AbstractDocumentRecord} stored in the repository which has the given <code>ID</code>.
+     * Returns a {@link AbstractDocumentRecord} stored in the repository which has the given ID.
      * 
      * @param loadFile
-     *            Indicator whether the <b>Blob</b> file will be loaded as well, or not.
+     *            If <code>true</code>, the <em>Blob</em> file will be loaded as well; otherwise not.
      */
     AbstractDocumentRecord findById(int documentId, boolean loadFile);
 
@@ -29,7 +31,7 @@ public interface DocumentRecordDao {
     AbstractDocumentRecord findByFileName(String fileName);
 
     /**
-     * Deletes the given <code>document</code> together with all its types from the repository.
+     * Deletes the given {@link AbstractDocumentRecord} together with all its types from the repository.
      */
     void delete(AbstractDocumentRecord document);
 }
