13b8be6305cf9da87c2862895ac8c2a1b50c98ab
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-01-04 18:22:46 +0100

    ThreadDaoTest class introduced, but not ready to use

---
+++ SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/repository/ThreadDaoTest.java
@@ -0,0 +1,15 @@
+package com.svnavigatoru600.repository;
+
+import org.junit.experimental.categories.Category;
+
+import com.svnavigatoru600.domain.forum.Thread;
+import com.svnavigatoru600.test.category.PersistenceTests;
+
+/**
+ * Tests methods of the {@link Thread} interface.
+ * 
+ * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
+ */
+@Category(PersistenceTests.class)
+public class ThreadDaoTest extends AbstractRepositoryTest {
+}
