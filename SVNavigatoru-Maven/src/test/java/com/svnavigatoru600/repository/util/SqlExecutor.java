4e3808542f111999c4ec36a39861403e0c5806ad
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-01-18 06:06:13 +0100

    NEW: usage of ibatis.jdbc.ScriptRunner -> SQL script comments to fit its requirements

--- SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/repository/util/SqlExecutor.java
+++ SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/repository/util/SqlExecutor.java
@@ -5,11 +5,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.ibatis.jdbc.ScriptRunner;
 
 /**
  * Contains convenient functions for a work with SQL.
@@ -41,14 +41,6 @@ public final class SqlExecutor {
     }
 
     /**
-     * Executes the given SQL statement in the schema determined by the given {@link Connection}.
-     */
-    public static void executeSqlStatement(String sqlStatement, Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        statement.execute(sqlStatement);
-    }
-
-    /**
      * Executes all code written in the given SQL file.
      * 
      * @param fileName
@@ -57,54 +49,16 @@ public final class SqlExecutor {
     public static void executeSqlFile(DataSource dataSource, String fileName) throws SQLException,
             IOException {
         Connection connection = null;
-        BufferedReader reader = null;
-
+        BufferedReader fileReader = null;
         try {
             connection = dataSource.getConnection();
-            reader = new BufferedReader(new FileReader(fileName));
+            ScriptRunner scriptRunner = new ScriptRunner(connection);
 
-            StringBuilder sqlBuilder = new StringBuilder();
-            boolean isInBlockComment = false;
-            for (String nextLine = reader.readLine(); nextLine != null; nextLine = reader.readLine()) {
-                // Ignores an inline comment which starts at the beginning of the current line.
-                if (nextLine.indexOf(SQL_INLINE_COMMENT_BEGINNING) == 0) {
-                    continue;
-                }
+            fileReader = new BufferedReader(new FileReader(fileName));
 
-                // Ignores a block comment which starts at the beginning of the current line.
-                int blockCommentStart = nextLine.indexOf(SQL_BLOCK_COMMENT_BEGINNING);
-                if (blockCommentStart == 0) {
-                    isInBlockComment = true;
-                }
-
-                // Ignores a block comment which ends up at the end of the current line.
-                int blockCommentEnd = nextLine.lastIndexOf(SQL_BLOCK_COMMENT_ENDING);
-                int commentEndIndexIfExists = nextLine.length() - SQL_BLOCK_COMMENT_ENDING.length();
-                boolean isLineEndBlockCommentEnd = (blockCommentEnd == commentEndIndexIfExists)
-                        && (blockCommentEnd >= 0);
-                if (isLineEndBlockCommentEnd) {
-                    isInBlockComment = false;
-                    continue;
-                }
-
-                // Ignores the current block comment.
-                if (isInBlockComment) {
-                    continue;
-                }
-
-                sqlBuilder.append(nextLine).append("\n");
-
-                // Executes all complete statements.
-                while (sqlBuilder.indexOf(SQL_COMMAND_ENDING) >= 0) {
-                    String sql = sqlBuilder.toString();
-                    int commandEndIndex = sql.indexOf(SQL_COMMAND_ENDING) + 1;
-                    String sqlStatement = sql.substring(0, commandEndIndex);
-                    executeSqlStatement(sqlStatement, connection);
-                    sqlBuilder.delete(0, commandEndIndex);
-                }
-            }
+            scriptRunner.runScript(fileReader);
         } finally {
-            IOUtils.closeQuietly(reader);
+            IOUtils.closeQuietly(fileReader);
             closeQuietly(connection);
         }
     }
