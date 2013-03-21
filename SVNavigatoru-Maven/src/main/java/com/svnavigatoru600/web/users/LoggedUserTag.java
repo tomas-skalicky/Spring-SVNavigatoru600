a07901d5e1f37330812cd88681f541c52d82b8e2
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-03-21 19:49:50 +0100

    tag library extended

--- SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/web/users/LoggedUserTag.java
+++ SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/web/users/LoggedUserTag.java
@@ -7,167 +7,201 @@ import com.svnavigatoru600.service.util.UserUtils;
 
 public class LoggedUserTag extends TagSupport {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private boolean checkIfCanEditNews = false;
-	private boolean checkIfCanEditEvents = false;
-	private boolean checkIfCanEditOtherDocumentRecords = false;
-	private boolean checkIfCanEditSessionRecords = false;
-	private boolean checkIfCanEditWysiwygSites = false;
-	private boolean checkIfCanSeeEvents = false;
-	private boolean checkIfCanSeeContributions = false;
-	private boolean checkIfCanSeeHomepage = false;
-	private boolean checkIfCanSeeUserAdministration = false;
+    private boolean checkIfCanEditNews = false;
+    private boolean checkIfCanEditEvents = false;
+    private boolean checkIfCanEditOtherDocumentRecords = false;
+    private boolean checkIfCanEditSessionRecords = false;
+    private boolean checkIfCanEditWysiwygSites = false;
+    private boolean checkIfCanSeeEvents = false;
+    private boolean checkIfCanSeeContributions = false;
+    private boolean checkIfCanSeeHomepage = false;
+    private boolean checkIfCanSeeNews = false;
+    private boolean checkIfCanSeeUserAdministration = false;
+    private boolean checkIfCanSeeHisAccount = false;
 
-	@Override
-	public int doStartTag() throws JspException {
-		if (cannotEditNewsThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotEditEventsThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotEditOtherDocumentRecordsThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotEditSessionRecordsThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotEditWysiwygSitesThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotSeeEventsThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotSeeContributionsThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotSeeHomepageThough()) {
-			return SKIP_BODY;
-		}
-		if (cannotSeeUserAdministrationThough()) {
-			return SKIP_BODY;
-		}
-		return EVAL_BODY_INCLUDE;
-	}
+    @Override
+    public int doStartTag() throws JspException {
+        if (cannotEditThough()) {
+            return SKIP_BODY;
+        }
+        if (cannotSeeThough()) {
+            return SKIP_BODY;
+        }
+        return EVAL_BODY_INCLUDE;
+    }
 
-	private boolean cannotEditNewsThough() {
-		return this.checkIfCanEditNews
-				&& !UserUtils.getLoggedUser().canEditNews();
-	}
+    private boolean cannotEditThough() {
+        if (cannotEditNewsThough()) {
+            return true;
+        }
+        if (cannotEditEventsThough()) {
+            return true;
+        }
+        if (cannotEditOtherDocumentRecordsThough()) {
+            return true;
+        }
+        if (cannotEditSessionRecordsThough()) {
+            return true;
+        }
+        if (cannotEditWysiwygSitesThough()) {
+            return true;
+        }
+        return false;
+    }
 
-	private boolean cannotEditEventsThough() {
-		return this.checkIfCanEditEvents
-				&& !UserUtils.getLoggedUser().canEditNews();
-	}
+    private boolean cannotEditNewsThough() {
+        return this.checkIfCanEditNews && !UserUtils.getLoggedUser().canEditNews();
+    }
 
-	private boolean cannotEditOtherDocumentRecordsThough() {
-		return this.checkIfCanEditOtherDocumentRecords
-				&& !UserUtils.getLoggedUser().canEditNews();
-	}
+    private boolean cannotEditEventsThough() {
+        return this.checkIfCanEditEvents && !UserUtils.getLoggedUser().canEditNews();
+    }
 
-	private boolean cannotEditSessionRecordsThough() {
-		return this.checkIfCanEditSessionRecords
-				&& !UserUtils.getLoggedUser().canEditNews();
-	}
+    private boolean cannotEditOtherDocumentRecordsThough() {
+        return this.checkIfCanEditOtherDocumentRecords && !UserUtils.getLoggedUser().canEditNews();
+    }
 
-	private boolean cannotEditWysiwygSitesThough() {
-		return this.checkIfCanEditWysiwygSites
-				&& !UserUtils.getLoggedUser().canEditNews();
-	}
+    private boolean cannotEditSessionRecordsThough() {
+        return this.checkIfCanEditSessionRecords && !UserUtils.getLoggedUser().canEditNews();
+    }
 
-	private boolean cannotSeeEventsThough() {
-		return this.checkIfCanSeeEvents
-				&& !UserUtils.getLoggedUser().canSeeNews();
-	}
+    private boolean cannotEditWysiwygSitesThough() {
+        return this.checkIfCanEditWysiwygSites && !UserUtils.getLoggedUser().canEditNews();
+    }
 
-	private boolean cannotSeeContributionsThough() {
-		return this.checkIfCanSeeContributions
-				&& !UserUtils.getLoggedUser().canSeeNews();
-	}
+    private boolean cannotSeeThough() {
+        if (cannotSeeEventsThough()) {
+            return true;
+        }
+        if (cannotSeeContributionsThough()) {
+            return true;
+        }
+        if (cannotSeeHomepageThough()) {
+            return true;
+        }
+        if (cannotSeeNewsThough()) {
+            return true;
+        }
+        if (cannotSeeUserAdministrationThough()) {
+            return true;
+        }
+        if (cannotSeeHisAccountThough()) {
+            return true;
+        }
+        return false;
+    }
 
-	private boolean cannotSeeHomepageThough() {
-		return this.checkIfCanSeeHomepage
-				&& !UserUtils.getLoggedUser().canSeeNews();
-	}
+    private boolean cannotSeeEventsThough() {
+        return this.checkIfCanSeeEvents && !UserUtils.getLoggedUser().canSeeNews();
+    }
 
-	private boolean cannotSeeUserAdministrationThough() {
-		return this.checkIfCanSeeUserAdministration
-				&& !UserUtils.getLoggedUser().canSeeUsers();
-	}
+    private boolean cannotSeeContributionsThough() {
+        return this.checkIfCanSeeContributions && !UserUtils.getLoggedUser().canSeeNews();
+    }
 
-	protected boolean isCheckIfCanEditNews() {
-		return this.checkIfCanEditNews;
-	}
+    private boolean cannotSeeHomepageThough() {
+        return this.checkIfCanSeeHomepage && !UserUtils.getLoggedUser().canSeeNews();
+    }
 
-	public void setCheckIfCanEditNews(boolean checkIfCanEditNews) {
-		this.checkIfCanEditNews = checkIfCanEditNews;
-	}
+    private boolean cannotSeeNewsThough() {
+        return this.checkIfCanSeeNews && !UserUtils.getLoggedUser().canSeeNews();
+    }
 
-	protected boolean isCheckIfCanEditEvents() {
-		return this.checkIfCanEditEvents;
-	}
+    private boolean cannotSeeUserAdministrationThough() {
+        return this.checkIfCanSeeUserAdministration && !UserUtils.getLoggedUser().canSeeUsers();
+    }
 
-	public void setCheckIfCanEditEvents(boolean checkIfCanEditEvents) {
-		this.checkIfCanEditEvents = checkIfCanEditEvents;
-	}
+    private boolean cannotSeeHisAccountThough() {
+        return this.checkIfCanSeeHisAccount && !UserUtils.getLoggedUser().canSeeHisAccount();
+    }
 
-	protected boolean isCheckIfCanEditOtherDocumentRecords() {
-		return this.checkIfCanEditOtherDocumentRecords;
-	}
+    protected boolean isCheckIfCanEditNews() {
+        return this.checkIfCanEditNews;
+    }
 
-	public void setCheckIfCanEditOtherDocumentRecords(
-			boolean checkIfCanEditOtherDocumentRecords) {
-		this.checkIfCanEditOtherDocumentRecords = checkIfCanEditOtherDocumentRecords;
-	}
+    public void setCheckIfCanEditNews(boolean checkIfCanEditNews) {
+        this.checkIfCanEditNews = checkIfCanEditNews;
+    }
 
-	protected boolean isCheckIfCanEditSessionRecords() {
-		return this.checkIfCanEditSessionRecords;
-	}
+    protected boolean isCheckIfCanEditEvents() {
+        return this.checkIfCanEditEvents;
+    }
 
-	public void setCheckIfCanEditSessionRecords(
-			boolean checkIfCanEditSessionRecords) {
-		this.checkIfCanEditSessionRecords = checkIfCanEditSessionRecords;
-	}
+    public void setCheckIfCanEditEvents(boolean checkIfCanEditEvents) {
+        this.checkIfCanEditEvents = checkIfCanEditEvents;
+    }
 
-	protected boolean isCheckIfCanEditWysiwygSites() {
-		return this.checkIfCanEditWysiwygSites;
-	}
+    protected boolean isCheckIfCanEditOtherDocumentRecords() {
+        return this.checkIfCanEditOtherDocumentRecords;
+    }
 
-	public void setCheckIfCanEditWysiwygSites(boolean checkIfCanEditWysiwygSites) {
-		this.checkIfCanEditWysiwygSites = checkIfCanEditWysiwygSites;
-	}
+    public void setCheckIfCanEditOtherDocumentRecords(boolean checkIfCanEditOtherDocumentRecords) {
+        this.checkIfCanEditOtherDocumentRecords = checkIfCanEditOtherDocumentRecords;
+    }
 
-	protected boolean isCheckIfCanSeeEvents() {
-		return this.checkIfCanSeeEvents;
-	}
+    protected boolean isCheckIfCanEditSessionRecords() {
+        return this.checkIfCanEditSessionRecords;
+    }
 
-	public void setCheckIfCanSeeEvents(boolean checkIfCanSeeEvents) {
-		this.checkIfCanSeeEvents = checkIfCanSeeEvents;
-	}
+    public void setCheckIfCanEditSessionRecords(boolean checkIfCanEditSessionRecords) {
+        this.checkIfCanEditSessionRecords = checkIfCanEditSessionRecords;
+    }
 
-	protected boolean isCheckIfCanSeeContributions() {
-		return this.checkIfCanSeeContributions;
-	}
+    protected boolean isCheckIfCanEditWysiwygSites() {
+        return this.checkIfCanEditWysiwygSites;
+    }
 
-	public void setCheckIfCanSeeContributions(boolean checkIfCanSeeContributions) {
-		this.checkIfCanSeeContributions = checkIfCanSeeContributions;
-	}
+    public void setCheckIfCanEditWysiwygSites(boolean checkIfCanEditWysiwygSites) {
+        this.checkIfCanEditWysiwygSites = checkIfCanEditWysiwygSites;
+    }
 
-	protected boolean isCheckIfCanSeeHomepage() {
-		return this.checkIfCanSeeHomepage;
-	}
+    protected boolean isCheckIfCanSeeEvents() {
+        return this.checkIfCanSeeEvents;
+    }
 
-	public void setCheckIfCanSeeHomepage(boolean checkIfCanSeeHomepage) {
-		this.checkIfCanSeeHomepage = checkIfCanSeeHomepage;
-	}
+    public void setCheckIfCanSeeEvents(boolean checkIfCanSeeEvents) {
+        this.checkIfCanSeeEvents = checkIfCanSeeEvents;
+    }
 
-	protected boolean isCheckIfCanSeeUserAdministration() {
-		return this.checkIfCanSeeUserAdministration;
-	}
+    protected boolean isCheckIfCanSeeContributions() {
+        return this.checkIfCanSeeContributions;
+    }
 
-	public void setCheckIfCanSeeUserAdministration(
-			boolean checkIfCanSeeUserAdministration) {
-		this.checkIfCanSeeUserAdministration = checkIfCanSeeUserAdministration;
-	}
+    public void setCheckIfCanSeeContributions(boolean checkIfCanSeeContributions) {
+        this.checkIfCanSeeContributions = checkIfCanSeeContributions;
+    }
+
+    protected boolean isCheckIfCanSeeHomepage() {
+        return this.checkIfCanSeeHomepage;
+    }
+
+    public void setCheckIfCanSeeHomepage(boolean checkIfCanSeeHomepage) {
+        this.checkIfCanSeeHomepage = checkIfCanSeeHomepage;
+    }
+
+    public boolean isCheckIfCanSeeNews() {
+        return this.checkIfCanSeeNews;
+    }
+
+    public void setCheckIfCanSeeNews(boolean checkIfCanSeeNews) {
+        this.checkIfCanSeeNews = checkIfCanSeeNews;
+    }
+
+    protected boolean isCheckIfCanSeeUserAdministration() {
+        return this.checkIfCanSeeUserAdministration;
+    }
+
+    public void setCheckIfCanSeeUserAdministration(boolean checkIfCanSeeUserAdministration) {
+        this.checkIfCanSeeUserAdministration = checkIfCanSeeUserAdministration;
+    }
+
+    public boolean isCheckIfCanSeeHisAccount() {
+        return this.checkIfCanSeeHisAccount;
+    }
+
+    public void setCheckIfCanSeeHisAccount(boolean checkIfCanSeeHisAccount) {
+        this.checkIfCanSeeHisAccount = checkIfCanSeeHisAccount;
+    }
 }
