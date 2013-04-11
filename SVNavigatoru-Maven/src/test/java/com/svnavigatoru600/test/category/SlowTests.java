8553aaec1a21f871047d5674167adba6d5776036
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-04-11 20:35:18 +0200

    old classes removed

--- SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/test/category/SlowTests.java
+++
@@ -1,9 +0,0 @@
-package com.svnavigatoru600.test.category;
-
-/**
- * Implemented by tests of which evaluation takes a fair amount of time (more than <code>1 sec</code>).
- * 
- * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
- */
-public interface SlowTests {
-}
