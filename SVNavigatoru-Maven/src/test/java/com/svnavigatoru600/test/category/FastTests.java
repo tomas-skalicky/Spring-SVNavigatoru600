b3f1b55855d8a5605eb1a8d461fcfcd1cb19c4cd
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-04-11 20:35:18 +0200

    old classes removed

--- SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/test/category/FastTests.java
+++
@@ -1,9 +0,0 @@
-package com.svnavigatoru600.test.category;
-
-/**
- * Implemented by tests of which evaluation is fast (<code>&lt;= 1 sec</code>).
- * 
- * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
- */
-public interface FastTests {
-}
