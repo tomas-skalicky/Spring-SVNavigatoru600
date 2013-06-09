ddf23ff479b276d71d6b9bfb63dc5b87cc522c07
Tomas Skalicky <skalicky.tomas@gmail.com>
2013-06-09 20:03:04 +0100

    do_release script fixed

--- SVNavigatoru-Maven/scripts/do_release.sh
+++ SVNavigatoru-Maven/scripts/do_release.sh
@@ -1,10 +1,13 @@
-# Tests whether all tests are satisfied (but Selenium ones).
-mvn clean compile
-mvn test -Dtests.category=com.svnavigatoru600.test.category.ControllerIntegrationTests
-mvn test -Dtests.category=com.svnavigatoru600.test.category.JsoupTests
-mvn test -Dtests.category=com.svnavigatoru600.test.category.PersistenceTests
-mvn test -Dtests.category=com.svnavigatoru600.test.category.UnitTests
-
-mvn -DskipTests release:prepare -Darguments="-DskipTests"
-mvn -DskipTests release:perform -Darguments="-DskipTests"
+# Tests whether all tests are satisfied (but Selenium ones) and then performs release.
+(cd ../ \
+&& mvn clean compile \
+&& mvn test -Dtests.category=com.svnavigatoru600.test.category.ControllerIntegrationTests \
+&& mvn test -Dtests.category=com.svnavigatoru600.test.category.JsoupTests \
+&& mvn test -Dtests.category=com.svnavigatoru600.test.category.PersistenceTests \
+&& mvn test -Dtests.category=com.svnavigatoru600.test.category.UnitTests \
+&& mvn -DskipTests release:prepare -Darguments="-DskipTests" \
+&& mvn -DskipTests release:perform -Darguments="-DskipTests" \
+&& echo -e "
+INFO: release has been SUCCESSFUL
+")
 
