76959c48a365277ab9b487ad86ec0cd0fe1e5d75
Tomas Skalicky <skalicky.tomas@gmail.com>
2014-03-30 21:40:01 +0200

    file limit set to 6 MB

--- SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/service/Configuration.java
+++ SVNavigatoru-Maven/src/main/java/com/svnavigatoru600/service/Configuration.java
@@ -21,10 +21,10 @@ public final class Configuration {
      */
     public static final String FILE_STORAGE = "uploaded-files/";
     /**
-     * The maximal size of a file which can be uploaded [in bytes]. <small>The current value equals 4
+     * The maximal size of a file which can be uploaded [in bytes]. <small>The current value equals 6
      * MB.</small>
      */
-    public static final long MAX_UPLOAD_SIZE = 4194304;
+    public static final long MAX_UPLOAD_SIZE = 6291456;
     /**
      * Default encoding for everything.
      */
