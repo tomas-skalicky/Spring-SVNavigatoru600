8a13f48a1c109700047b6e9928825965cbeb755c
Tomas Skalicky <skalicky.tomas@gmail.com>
2014-05-01 13:20:27 +0200

    fixed do_release: - JDK 6 or JDK 7 necessary - line endings cannot contain \r

--- SVNavigatoru-Maven/scripts/do_release.sh
+++ SVNavigatoru-Maven/scripts/do_release.sh
@@ -1,13 +1,27 @@
-# Tests whether all tests are satisfied (but Selenium ones) and then performs release.
+# JDK 8 starts a javadoc check which fails -> we need JDK 6 or JDK 7
+JAVA_HOME_BACKUP="$JAVA_HOME"
+
+if [ -n	"$JDK7_HOME" ]; then
+	JAVA_HOME="$JDK7_HOME"
+else 
+	echo -e "\nERROR: you have to set JDK7_HOME environment variable.\n"
+	exit -1
+fi
+echo "INFO: Current JAVA_HOME is $JAVA_HOME"
+
+
+# Tests whether all unit and integration tests are satisfied (except availability and Selenium tests)
+# If so, it performs release.
 (cd ../ \
-&& mvn clean compile \
-&& mvn test -Dtests.category=com.svnavigatoru600.test.category.ControllerIntegrationTests \
-&& mvn test -Dtests.category=com.svnavigatoru600.test.category.JsoupTests \
-&& mvn test -Dtests.category=com.svnavigatoru600.test.category.PersistenceTests \
-&& mvn test -Dtests.category=com.svnavigatoru600.test.category.UnitTests \
-&& mvn -DskipTests release:prepare -Darguments="-DskipTests" \
-&& mvn -DskipTests release:perform -Darguments="-DskipTests" \
-&& echo -e "
-INFO: release has been SUCCESSFUL
-")
+	&& mvn clean compile \
+	&& mvn test -Dtests.category=com.svnavigatoru600.test.category.ControllerIntegrationTests \
+	&& mvn test -Dtests.category=com.svnavigatoru600.test.category.PersistenceTests \
+	&& mvn test -Dtests.category=com.svnavigatoru600.test.category.UnitTests \
+	&& mvn -DskipTests release:prepare -Darguments="-DskipTests" \
+	&& mvn -DskipTests release:perform -Darguments="-DskipTests" \
+	&& echo -e "\nINFO: release has been SUCCESSFUL\n")
+
+
+# Sets the JAVA_HOME environment variable back.
+JAVA_HOME="$JAVA_HOME_BACKUP"
 
