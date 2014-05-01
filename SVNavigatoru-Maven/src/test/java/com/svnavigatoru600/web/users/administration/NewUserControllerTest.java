5c13ca57e2bd9eb4f4a5c28f192fab66020a0e6f
Tomas Skalicky <skalicky.tomas@gmail.com>
2014-05-01 10:45:58 +0200

    formatted

--- SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/web/users/administration/NewUserControllerTest.java
+++ SVNavigatoru-Maven/src/test/java/com/svnavigatoru600/web/users/administration/NewUserControllerTest.java
@@ -25,6 +25,7 @@ import com.svnavigatoru600.service.users.UserService;
 import com.svnavigatoru600.test.category.ControllerIntegrationTests;
 import com.svnavigatoru600.url.users.UserAdministrationUrlParts;
 import com.svnavigatoru600.viewmodel.users.validator.NewUserValidator;
+import com.svnavigatoru600.web.AbstractPrivateSectionMetaController;
 
 /**
  * @author <a href="mailto:skalicky.tomas@gmail.com">Tomas Skalicky</a>
@@ -36,6 +37,28 @@ public final class NewUserControllerTest {
 
     private TestContextManager testContextManager;
 
+    private MockMvc mockMvc;
+
+    private NewUserController controller;
+
+    @Mock
+    private CalendarEventService calendarEventService;
+
+    @Mock
+    private ContributionService contributionService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private MessageSource messageSource;
+
+    @Mock
+    private NewUserValidator validator;
+
+    @Mock
+    private AuthorityService authorityService;
+
     @Before
     public void setUpContext() throws Exception {
         // This is where the magic happens. We actually do "by hand" what the
@@ -44,48 +67,50 @@ public final class NewUserControllerTest {
         this.testContextManager.prepareTestInstance(this);
     }
 
-    private MockMvc mockMvc;
-    private NewUserController controller;
-
     @Before
     public void initMockMvc() {
-        this.controller = new NewUserController();
-        this.mockMvc = standaloneSetup(this.controller).build();
         initMocks();
-    }
 
-    @Mock
-    private CalendarEventService calendarEventService;
-    @Mock
-    private ContributionService contributionService;
-    @Mock
-    private UserService userService;
-    @Mock
-    private MessageSource messageSource;
-    @Mock
-    private NewUserValidator validator;
-    @Mock
-    private AuthorityService authorityService;
+        this.controller = new NewUserController();
+        mockControllerModelAttributes(this.controller);
+        mockNewUserControllerServices(this.controller);
+
+        this.mockMvc = standaloneSetup(this.controller).build();
+    }
 
     private void initMocks() {
         MockitoAnnotations.initMocks(this);
-        this.controller.setCalendarEventService(this.calendarEventService);
-        this.controller.setContributionService(this.contributionService);
-        this.controller.setUserService(this.userService);
-        this.controller.setMessageSource(this.messageSource);
-        this.controller.setValidator(this.validator);
-        this.controller.setAuthorityService(this.authorityService);
+    }
+
+    private void mockControllerModelAttributes(AbstractPrivateSectionMetaController controller) {
+        controller.setCalendarEventService(this.calendarEventService);
+        controller.setContributionService(this.contributionService);
+        controller.setUserService(this.userService);
+    }
+
+    private void mockNewUserControllerServices(NewUserController controller) {
+        controller.setMessageSource(this.messageSource);
+        controller.setValidator(this.validator);
+        controller.setAuthorityService(this.authorityService);
     }
 
     @Test
     public void testInitForm() throws Exception {
+
+        // Mocks calls to business logic
         when(this.userService.getLoggerUser()).thenReturn(null);
 
-        this.mockMvc.perform(get(UserAdministrationUrlParts.NEW_URL).accept(MediaType.TEXT_HTML))
-                .andExpect(status().isOk()).andExpect(forwardedUrl("newUser"))
-                .andExpect(model().errorCount(0)).andExpect(model().attributeExists("userCommand"))
+        // @formatter:off
+        this.mockMvc.perform(get(UserAdministrationUrlParts.NEW_URL)
+                .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("newUser"))
+                .andExpect(model().errorCount(0))
+                .andExpect(model().attributeExists("userCommand"))
                 .andDo(print());
+        // @formatter:on
 
+        // Verifies mocked calls
         verify(this.userService).getLoggerUser();
     }
 }
