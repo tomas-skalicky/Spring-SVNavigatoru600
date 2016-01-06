e19420d48478dc41d2df7b85afb0e65695daba8c
Tomas Skalicky <skalicky.tomas@gmail.com>
2016-01-06 19:57:58 +0100

    cleanup of bash scripts

--- SVNavigatoru-Maven/scripts/assemble_production_package.bash
+++ SVNavigatoru-Maven/scripts/assemble_production_package.bash
@@ -10,45 +10,45 @@
 set -e
 
 # JDK 8 starts a javadoc check which fails -> we need JDK 6 or JDK 7
-JAVA_HOME_BACKUP="$JAVA_HOME"
+JAVA_HOME_BACKUP=$JAVA_HOME
 
-if [ -z "$JDK7_HOME" ]; then
+if [[ -z $JDK7_HOME ]]; then
     JDK7_HOME=/usr/lib/jvm/jdk7
 fi
-JAVA_HOME="$JDK7_HOME"
+JAVA_HOME=$JDK7_HOME
 echo "INFO: Current JAVA_HOME is $JAVA_HOME"
 
 
 
-PROJECT_HOME=".."
-cd -P $PROJECT_HOME
-PROJECT_HOME=`pwd`
+PROJECT_HOME=..
+cd -P "$PROJECT_HOME"
+PROJECT_HOME=$(pwd)
 
 mvn clean package -DskipTests
 
-BUILD_HOME="$PROJECT_HOME/target"
-cd $BUILD_HOME
+BUILD_HOME=$PROJECT_HOME/target
+cd "$BUILD_HOME"
 
 
 # Renames the package to the target name.
 MAVEN_PACKAGE_NAME=svnavigatoru600*.war
-TARGET_PACKAGE_NAME="ROOT.war"
-mv $MAVEN_PACKAGE_NAME $TARGET_PACKAGE_NAME
+TARGET_PACKAGE_NAME=ROOT.war
+mv "$MAVEN_PACKAGE_NAME" "$TARGET_PACKAGE_NAME"
 
 
 # Unpacks the output package of the maven.
-TWEAK_DIR="ROOT.war_tweak"
-mkdir $TWEAK_DIR
-cd $TWEAK_DIR
-jar xf ../$TARGET_PACKAGE_NAME
+TWEAK_DIR=ROOT.war_tweak
+mkdir "$TWEAK_DIR"
+cd "$TWEAK_DIR"
+jar xf ../"$TARGET_PACKAGE_NAME"
 
 # Sets up the production JDBC configuration instead of the development one.
-PATH_TO_JDBC_CONFIG="WEB-INF/classes"
-ACTIVE_JDBC_CONFIG="$PATH_TO_JDBC_CONFIG/jdbc.properties"
-PRODUCTION_JDBC_CONFIG="$PATH_TO_JDBC_CONFIG/jdbc-production.properties"
-mv $PRODUCTION_JDBC_CONFIG $ACTIVE_JDBC_CONFIG
+PATH_TO_JDBC_CONFIG=WEB-INF/classes
+ACTIVE_JDBC_CONFIG=$PATH_TO_JDBC_CONFIG/jdbc.properties
+PRODUCTION_JDBC_CONFIG=$PATH_TO_JDBC_CONFIG/jdbc-production.properties
+mv "$PRODUCTION_JDBC_CONFIG" "$ACTIVE_JDBC_CONFIG"
 
 # Packs all files to create the desired output WAR file.
-jar cf $TARGET_PACKAGE_NAME *
-mv $TARGET_PACKAGE_NAME $BUILD_HOME/
+jar cf "$TARGET_PACKAGE_NAME" *
+mv "$TARGET_PACKAGE_NAME" "$BUILD_HOME"/
 
--- SVNavigatoru-Maven/scripts/do_release.bash
+++ SVNavigatoru-Maven/scripts/do_release.bash
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
 
 # JDK 8 starts a javadoc check which fails -> we need JDK 6 or JDK 7
-JAVA_HOME_BACKUP="$JAVA_HOME"
+JAVA_HOME_BACKUP=$JAVA_HOME
 
-if [ -n	"$JDK7_HOME" ]; then
-	JAVA_HOME="$JDK7_HOME"
-else 
-	echo -e "\nERROR: you have to set JDK7_HOME environment variable.\n"
-	exit -1
+if [[ -z $JDK7_HOME ]]; then
+    JDK7_HOME=/usr/lib/jvm/jdk7
 fi
+JAVA_HOME=$JDK7_HOME
 echo "INFO: Current JAVA_HOME is $JAVA_HOME"
 
 
 # Tests whether all unit and integration tests are satisfied (except availability and Selenium tests)
 # If so, it performs release.
 (cd ../ \
-	&& mvn clean compile \
-	&& mvn test -Dtests.category=com.svnavigatoru600.test.category.ControllerIntegrationTests \
-	&& mvn test -Dtests.category=com.svnavigatoru600.test.category.PersistenceTests \
-	&& mvn test -Dtests.category=com.svnavigatoru600.test.category.UnitTests \
-	&& mvn -DskipTests release:prepare -Darguments="-DskipTests" \
-	&& mvn -DskipTests release:perform -Darguments="-DskipTests" \
-	&& echo -e "\nINFO: release has been SUCCESSFUL\n")
+    && mvn clean compile \
+    && mvn test -Dtests.category=com.svnavigatoru600.test.category.ControllerIntegrationTests \
+    && mvn test -Dtests.category=com.svnavigatoru600.test.category.PersistenceTests \
+    && mvn test -Dtests.category=com.svnavigatoru600.test.category.UnitTests \
+    && mvn -DskipTests release:prepare -Darguments=-DskipTests \
+    && mvn -DskipTests release:perform -Darguments=-DskipTests \
+    && echo -e '\nINFO: release has been SUCCESSFUL\n')
 
 
 # Sets the JAVA_HOME environment variable back.
-JAVA_HOME="$JAVA_HOME_BACKUP"
+JAVA_HOME=$JAVA_HOME_BACKUP
 
